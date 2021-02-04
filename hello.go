6d69d1787258a48d91a27a53959f48aae7e86dd3
xingmegshuo <xms.chnb@gmail.com>
2021-02-04 16:20:00 +0800

    hello

---
+++ hello.go
@@ -0,0 +1,15 @@
+/***************************
+@File        : hello.go
+@Time        : 2021/02/04 16:19:05
+@AUTHOR      : small_ant
+@Email       : xms.chnb@gmail.com
+@Desc        : test
+****************************/
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+}
